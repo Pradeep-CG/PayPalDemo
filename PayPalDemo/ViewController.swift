b4073aeb38461efd0cd6f7ca21be3b5e69ed80e7
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-05-12 20:19:47 +0530

    first commit

--- PayPalDemo/ViewController.swift
+++ PayPalDemo/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
-
+import Braintree
+/*
+ braintree & paypal acc:
+ pradeep.sagar.ios@gmail.com/ sagar9718
+ 
+ account: sb-rl5zw1748613@personal.example.com
+ client id: Ad_9DuwzUk1v-AnZyHSxUBTCI9uK3qfTLbDPGgDYV8lfG4AlMKSaVfD_lt-zk8vt-3rBQ3f3_F9N_mdC
+ secret: EHovYC5wGSkziZLXWMUoaWOnUip3JoFqBV-ZiF352SBR2SWUaaT1_FYNtPa8lBBBjxX-CrGPNk70pCtu
+ tokenization key: sandbox_mfh8zzzh_rjjz8dmq92vs7qmg
+ 
+ */
 class ViewController: UIViewController {
 
+    var braintreeClient: BTAPIClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        braintreeClient = BTAPIClient(authorization: "sandbox_mfh8zzzh_rjjz8dmq92vs7qmg")!
+        
+        
     }
 
+    @IBAction func onPayClicked(_ sender: Any) {
+        
+        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
+        payPalDriver.viewControllerPresentingDelegate = self
+        payPalDriver.appSwitchDelegate = self // Optional
+        
+        // Specify the transaction amount here. "2.32" is used in this example.
+        let request = BTPayPalRequest(amount: "2.32")
+        request.currencyCode = "INR" // Optional; see BTPayPalRequest.h for more options
 
+        payPalDriver.requestOneTimePayment(request) { (tokenizedPayPalAccount, error) in
+            if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+                print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+
+                // Access additional information
+                let email = tokenizedPayPalAccount.email
+                let firstName = tokenizedPayPalAccount.firstName
+                let lastName = tokenizedPayPalAccount.lastName
+                let phone = tokenizedPayPalAccount.phone
+
+                // See BTPostalAddress.h for details
+                let billingAddress = tokenizedPayPalAccount.billingAddress
+                let shippingAddress = tokenizedPayPalAccount.shippingAddress
+            } else if let error = error {
+                // Handle error here...
+            } else {
+                // Buyer canceled payment approval
+            }
+        }
+    }
+    
+}
+extension ViewController: BTViewControllerPresentingDelegate {
+    func paymentDriver(_ driver: Any, requestsPresentationOf viewController: UIViewController) {
+        
+    }
+    
+    func paymentDriver(_ driver: Any, requestsDismissalOf viewController: UIViewController) {
+        
+    }
+    
+    
+}
+extension ViewController: BTAppSwitchDelegate{
+    func appSwitcherWillPerformAppSwitch(_ appSwitcher: Any) {
+        
+    }
+    
+    func appSwitcher(_ appSwitcher: Any, didPerformSwitchTo target: BTAppSwitchTarget) {
+        
+    }
+    
+    func appSwitcherWillProcessPaymentInfo(_ appSwitcher: Any) {
+        
+    }
+    
+    
 }
 
